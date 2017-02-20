1c6b83074c4b9c9bfe412fe644acc03a1fb61b20
dongxiuling <dxl_web@163.com>
2017-02-20 10:30:23 +0800

    require

--- requirejs/弹出层/js/dialog2.js
+++ requirejs/弹出层/js/dialog2.js
@@ -2,55 +2,45 @@
  * Created by apple on 17/2/19.
  */
 define(['jquery'],function($){
-    var $container = $('<div class="dialog-container"></div>'),
-        $mask = $('<div class="dialog-mask"></div>'),
-        $box = $('<div class="dialog-box"></div>'),
-        $titleBox = $('<div class="dialog-title-box"></div>'),
-        $title = $('<div class="dialog-title"></div>'),
-        $close = $('<div class="dialog-close">[X]</div>'),
-        $content = $('<div class="dialog-content"></div>');
 
-    return {
-        defaultVal:{
+    function Dialog(){
+        this.defaultVal = {
             width:400,
-            height:300
-        },
+                height:300
+        };
+    }
 
-        open:function(options){
-            var _this = this;
-            $.extend(this.defaultVal,options);
-           // console.log(this.defaultVal); //{width: 800, height: 500, title: "呵呵", content: "hjsgdkhag "}
+    Dialog.prototype.open = function(options){
+        this.$container = $('<div class="dialog-container"></div>');
+        this.$mask = $('<div class="dialog-mask"></div>');
+        this.$box = $('<div class="dialog-box"></div>');
+        this.$titleBox = $('<div class="dialog-title-box"></div>');
+        this.$title = $('<div class="dialog-title"></div>');
+        this.$close = $('<div class="dialog-close">[X]</div>');
+        this.$content = $('<div class="dialog-content"></div>');
 
-
-           /* var str = '<div class="dialog-container">'
-                        +'<div class="dialog-mask"></div>'
-                        +'<div class="dialog-box">'
-                        +'<div class="dialog-title-box">'
-                        +'<div class="dialog-title">'+this.defaultVal.title+'</div>'
-                        +'<div class="dialog-close">[X]</div>'
-                        +'</div>'
-                        +'<div class="dialog-content">'+this.defaultVal.content+'</div>'
-                        +'</div>'
-                        +'</div>';*/
-            $container.append($mask).append($box);
-            $box.append($titleBox).append($content.html($('.dialog-box-content').show()))
-                .css({
+        var _this = this;
+        $.extend(this.defaultVal,options);
+        this.$container.append(this.$mask).append(this.$box);
+        this.$box.append(this.$titleBox).append(this.$content.html($('.dialog-box-content').show()))
+            .css({
                 width:this.defaultVal.width,
                 height:this.defaultVal.height,
                 'margin-left':-this.defaultVal.width/2,
                 'margin-top':-this.defaultVal.height/2
             });
 
-            $titleBox.append($title.html(this.defaultVal.title)).append($close);
-            $('body').append($container);
+        this.$titleBox.append(this.$title.html(this.defaultVal.title)).append(this.$close);
+        $('body').append(this.$container);
 
-            $close.on('click',function(){
-                _this.close();
-            });
-        },
-        close:function(){
-            $container.remove();
-        }
-    }
+        this.$close.on('click',function(){
+            _this.close();
+        });
+    };
+    Dialog.prototype.close = function(){
+        this.$container.remove();
+    };
+
+    return Dialog;
 
 });
--- requirejs/弹出层/js/index.js
+++ requirejs/弹出层/js/index.js
@@ -1,12 +1,15 @@
-require(['jquery','dialog2'],function($,dialog){
+require(['jquery','dialog2'],function($,Dialog){
     $('#btn').on('click',function(){
-        dialog.open({
+        /*dialog.open({
             width:500,
             height:400,
             title:'呵呵',
             content:'dialog-box-content'
 
-        });
+        });*/
+
+        var dialog = new Dialog();
+        dialog.open();
 
         //dialog.close();
     });
