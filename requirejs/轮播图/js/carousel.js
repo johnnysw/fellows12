006db81140f0d1212ded650df6f6a92ad95531a8
dongxiuling <dxl_web@163.com>
2017-02-20 15:59:08 +0800

    require

--- requirejs/轮播图/js/carousel.js
+++ requirejs/轮播图/js/carousel.js
@@ -16,6 +16,8 @@ define(['jquery'],function($){
 
     }
     Carousel.prototype.init = function(options){
+        var iNow = 0;
+        var _this = this;
         $.extend(this.defaultVal,options);
 
         this.$container.append(this.$content).append(this.$tab).append(this.$arrow);
@@ -26,15 +28,59 @@ define(['jquery'],function($){
             if(i == 0){
                 imgClass = 'selected';
             }
-            imgClass = i == 0?'selected':'';*/
+            imgClass = (i == 0?'selected':'');*/
             this.$content.append('<img class="'+(i == 0?'selected':'')+'" src="'+this.defaultVal.imgData[i]+'"/>');
-            this.$tab.append('<li class="'+(i == 0?'selected':'')+'">'+(i+1)+'</li>');
+            this.$tab.append('<li class="'+this.defaultVal.buttonStyle+(i == 0?' selected':'')+'">'+(i+1)+'</li>');
         }
 
 
         this.$arrow.append(this.$prev).append(this.$next);
 
         $(this.defaultVal.selector).append(this.$container);
+
+        $('li',this.$tab).on('click',function(){
+            //console.log(11111);
+            changeImg($(this).index());
+        });
+
+
+        function changeImg(idx){
+
+            //console.log(this.$tab);
+            $('li',_this.$tab).eq(idx).addClass('selected').siblings().removeClass('selected');
+            $('img',_this.$content).eq(idx).addClass('selected').siblings().removeClass('selected');
+        }
+
+        this.$prev.on('click',function(){
+            iNow--;
+            if(iNow == -1){
+                iNow = _this.defaultVal.imgData.length-1;
+            }
+
+            changeImg(iNow);
+        });
+        this.$next.on('click',function(){
+            iNow++;
+            if(iNow == _this.defaultVal.imgData.length){
+                iNow = 0;
+            }
+
+            changeImg(iNow);
+        });
+        var timer;
+        function run(){
+
+            timer = setInterval(function(){
+                _this.$next.trigger('click');
+            },_this.defaultVal.speed);
+        }
+        run();
+
+        this.$container.hover(function(){
+            clearInterval(timer);
+        },function(){
+            run();
+        });
 
     }
 
